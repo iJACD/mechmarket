116df72cd84a0aba400b28c42f3fca8bc6eb34af
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-06-05 13:38:27 -0400

    added constants file

---
+++ mechmarket/Constants/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/5/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import UIKit
+
+public struct MK {
+    struct OriginPage {
+        static let imageOverlayText = "r/mk"
+        static let selectOriginText = "Select search origin: "
+    }
+    
+    struct Images {
+        static let keyBoardImage = UIImage(named: "keyboardImage")
+    }
+    
+    struct FontNamed {
+        static let HelveticaBold = "Helvetica-Bold"
+        static let HelveticaLight = "Helvetica-Light"
+    }
+}
---
+++ mechmarket/Views/CountryButtonCell.swift
@@ -0,0 +1,9 @@
+//
+//  CountryButtonCell.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/5/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
