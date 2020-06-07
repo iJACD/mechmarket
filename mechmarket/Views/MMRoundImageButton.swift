cecc82f3f8903f808f4fdbf5c5778ed0d949a1d2
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-06-07 02:10:26 -0400

    made round image button a custom button class. started building the header view.

---
+++ mechmarket/Views/MMRoundImageButton.swift
@@ -0,0 +1,21 @@
+//
+//  MMRoundImageButton.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/7/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import UIKit
+
+class MMRoundImageButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
