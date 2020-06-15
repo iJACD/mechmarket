91d8f5effceb319be57278ac2cf35321a991fa04
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-06-15 00:19:21 -0400

    cleaned up and organized some code. added some extensions and logic to help fix the imcoming url for imgur images.

---
+++ mechmarket/Extensions/String+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  String+Extensions.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/15/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
---
+++ mechmarket/Views/MMCategoryButton.swift
@@ -0,0 +1,9 @@
+//
+//  MMCategoryButton.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/15/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
