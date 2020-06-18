ad1074c12ada3916d53d6d4cc7818d196e407cd1
JohnAnthony Dobson <jcdobson2@gmail.com>
2020-06-18 01:06:09 -0400

    added tag label to collection view cells, refactored some varaibles, upated some ui. save

---
+++ mechmarket/Views/MMFlairTagLabel.swift
@@ -0,0 +1,9 @@
+//
+//  MMFlairTagLabel.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/18/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
---
+++ mechmarket/Views/MMSellTradeCollectionCell.swift
@@ -0,0 +1,9 @@
+//
+//  SellTradeCollectionCell.swift
+//  mechmarket
+//
+//  Created by JohnAnthony on 6/16/20.
+//  Copyright © 2020 iJACD. All rights reserved.
+//
+
+import Foundation
